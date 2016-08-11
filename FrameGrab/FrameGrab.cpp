92aadeac03b975e15057cff4cd41d69256845689
rkyoung3 <rkyoung@sonic.net>
2016-08-11 16:31:59 -0700

    Initial Version - 0.0.1

--- FrameGrab/FrameGrab.cpp
+++ FrameGrab/FrameGrab.cpp
@@ -1,15 +1,61 @@
+// FrameGrab  - Created: 08/11/2016  8/11/2016 3:25:59 PM
+// Creator Robert K Young - rkyoung@sonic.net
+// ChangeLog:
+// 0.0.1 - 8/11/2016 3:32:33 PM - Initial Version
+
 // FrameGrab.cpp : Defines the entry point for the application.
 //
 
 #include "stdafx.h"
 #include "FrameGrab.h"
 
+// Contributing Source Used: http://www.dreamincode.net/forums/topic/193519-win32-webcam-program/
+//
+#include "windows.h"
+#include "vfw.h"
+
 #define MAX_LOADSTRING 100
+
+#define WM_CAP_START  0x0400  
+
+#define WM_CAP_DRIVER_CONNECT (WM_CAP_START + 10)
+
+#define WM_CAP_DRIVER_DISCONNECT (WM_CAP_START + 11)
+
+#define WM_CAP_EDIT_COPY (WM_CAP_START + 30)
+
+#define WM_CAP_GRAB_FRAME (WM_CAP_START + 60)
+
+#define WM_CAP_SET_SCALE (WM_CAP_START + 53)
+
+#define WM_CAP_SET_PREVIEWRATE (WM_CAP_START + 52)
+
+#define WM_CAP_SET_PREVIEW (WM_CAP_START + 50)
+
+#define WM_CAP_DLG_VIDEOSOURCE (WM_CAP_START + 42)
+
+#define WM_CAP_STOP (WM_CAP_START + 68)
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+
+PBITMAPINFO CreateBitmapInfoStruct(HWND hwnd, HBITMAP hBmp);
+
+void CreateBMPFile(HWND hwnd, LPTSTR pszFile, PBITMAPINFO pbi, HBITMAP hBMP, HDC hDC);
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+LPCTSTR szAppName = L"FrameGrab";
+WNDCLASS wc;
+HWND camhwnd;
+HDC hdc;
+HDC hdcMem;
+PAINTSTRUCT ps;
+HBITMAP hbm;
+RECT rc;
+
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -17,10 +63,7 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -31,6 +74,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_FRAMEGRAB, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
+
+	HWND hwnd;
+
+	MSG msg;
 
     // Perform application initialization:
     if (!InitInstance (hInstance, nCmdShow))
@@ -97,16 +144,32 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   
+   wc.style = CS_HREDRAW | CS_VREDRAW;
+   wc.lpfnWndProc = WindowProc;
+   wc.cbClsExtra = 0;
+   wc.cbWndExtra = 0;
+   wc.hInstance = hInstance;
+   wc.hIcon = LoadIcon(GetModuleHandle(NULL), IDI_APPLICATION);
+   wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+   wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+   wc.lpszMenuName = NULL;
+   wc.lpszClassName = szAppName;
 
-   if (!hWnd)
+
+   RegisterClass(&wc);
+
+   // Create the window  
+
+   camhwnd = CreateWindow(szAppName, szAppName, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 300, 600, 0, 0, hInstance, 0);
+
+   if (!camhwnd)
    {
       return FALSE;
    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+   ShowWindow(camhwnd, nCmdShow);
+   UpdateWindow(camhwnd);
 
    return TRUE;
 }
